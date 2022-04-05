6dbf4ab5ee2e7aff9c8ea616bb6dce7d2196cfc3
Khalid Grandi <kh.grandi@gmail.com>
2022-04-05 23:48:14 +0100

    renaming flaskly to ElementaryFlask

--- project_name/_app.py
+++ project_name/_app.py
@@ -1,17 +1,17 @@
 from os.path import abspath, basename, join
 
-from flaskly import FlasklyApp
+from elementary_flask import ElementaryFlask
 
 module_parent = basename(basename(abspath(__file__)))
 template_folder, static_folder = join(module_parent, 'templates'), join(module_parent, 'static')
 PROJECT_NAME = 'PROJECT_NAME'
 PROJECT_TITLE = 'PROJECT_NAME'
 
-app = FlasklyApp(PROJECT_NAME,
-                 secret='Secretlkjlkdsjhfdsgfd54gf56d456gd4564hgf65h46d4fh',
-                 default_title=PROJECT_TITLE,
-                 template_folder=template_folder,
-                 static_folder=static_folder,
-                 theme="adminlte"
+app = ElementaryFlask(PROJECT_NAME,
+                      secret='Secretlkjlkdsjhfdsgfd54gf56d456gd4564hgf65h46d4fh',
+                      default_title=PROJECT_TITLE,
+                      template_folder=template_folder,
+                      static_folder=static_folder,
+                      theme="adminlte"
 
-                 )
+                      )
